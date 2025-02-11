50728e523d922cd1c2a0d339851bc936ab07015c
Hpsaturn <hpsaturn@gmail.com>
2025-02-11 21:36:49 +0100

    improve clear screen using subrutine

--- examples/dcf77_clock/dcf77_clock.asm
+++ examples/dcf77_clock/dcf77_clock.asm
@@ -1,17 +1,20 @@
 ; DCF77Clock
 ; @hpsaturn 2025
 
-
+init:
 mov r0, 2        ; Move to page 2
 mov [0xF0], r0
 
+; mov r0, 4   ; Set CPU speed
+; mov [0xF1], r0
+
 main:     ; The main loop of the program.
+gosub pulse_off
 mov r1, 0       ; Zero out R1
 mov r0, [0x0B]  ; Read input pin status into R0
 bit r0, 1       ; If first bit of R0 is 0, set Z flag
 skip nz, 1      ; Skip next line if Z is NOT set
 goto pulse_on
-goto clear_disp
 goto main
 
 clear_disp:
@@ -27,21 +30,17 @@ mov [r4:r5], r0
 inc r5
 dsz r7
 jr -5
-goto main
+
+ret r0, 0
+
+pulse_off:
+gosub clear_disp
+ret r0, 0
 
 pulse_on:
 ; clear screen first
 ; blank the matrix first for a fresh draw
-mov r0, 0
-mov r7, 0
-mov r5, 0
-mov r3, 2
-mov r4, 3
-mov [r3:r5], r0
-mov [r4:r5], r0
-inc r5
-dsz r7
-jr -5
+gosub clear_disp
 
 mov r5, 0b0011   ; Left side of icon 
 mov r6, 0b1100   ; Right side of icon
@@ -62,6 +61,8 @@ mov [3:11], r0   ;
 mov [2:11], r0   ;
 
 goto main
+
+
 
 ; mov r1, 0       ; Zero out R1
 ; mov r0, [0x0B]  ; Read input pin status into R0
