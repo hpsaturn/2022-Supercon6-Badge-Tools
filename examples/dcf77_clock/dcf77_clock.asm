a3d20041a046a126b2dcf78e12853a9a58b3eb3b
Hpsaturn <hpsaturn@gmail.com>
2025-02-11 20:16:11 +0100

    working pin1 to 0

--- examples/dcf77_clock/dcf77_clock.asm
+++ examples/dcf77_clock/dcf77_clock.asm
@@ -1,12 +1,70 @@
 ; DCF77Clock
 ; @hpsaturn 2025
 
+
+mov r0, 2        ; Move to page 2
+mov [0xF0], r0
+
 main:     ; The main loop of the program.
-
-
-gpio_loop:
 mov r1, 0       ; Zero out R1
 mov r0, [0x0B]  ; Read input pin status into R0
 bit r0, 1       ; If first bit of R0 is 0, set Z flag
 skip nz, 1      ; Skip next line if Z is NOT set
-mov r1, 15      ; Put 15 into R1
+goto pulse_on
+goto clear_disp
+goto main
+
+clear_disp:
+; clear screen first
+; blank the matrix first for a fresh draw
+mov r0, 0
+mov r7, 0
+mov r5, 0
+mov r3, 2
+mov r4, 3
+mov [r3:r5], r0
+mov [r4:r5], r0
+inc r5
+dsz r7
+jr -5
+goto main
+
+pulse_on:
+; clear screen first
+; blank the matrix first for a fresh draw
+mov r0, 0
+mov r7, 0
+mov r5, 0
+mov r3, 2
+mov r4, 3
+mov [r3:r5], r0
+mov [r4:r5], r0
+inc r5
+dsz r7
+jr -5
+
+mov r5, 0b0011   ; Left side of icon 
+mov r6, 0b1100   ; Right side of icon
+
+mov r2, 2        ; Register for page 2
+mov r3, 3        ; Register for page 3
+mov r4, 14       ; Start at row 14
+
+mov r0, r5       ; Get left side data into R0
+mov [r3:r4], r0  ; Draw left side
+mov r0, r6       ; Get right side data into R0
+mov [r2:r4], r0  ; Draw right side
+dsz r4           ; Decrement row
+jr -6            ; Draw 14 rows
+
+mov r0, 0        ; Clear line at bottom
+mov [3:11], r0   ;  
+mov [2:11], r0   ;
+
+goto main
+
+; mov r1, 0       ; Zero out R1
+; mov r0, [0x0B]  ; Read input pin status into R0
+; bit r0, 1       ; If first bit of R0 is 0, set Z flag
+; skip nz, 1      ; Skip next line if Z is NOT set
+; mov r1, 15      ; Put 15 into R1
